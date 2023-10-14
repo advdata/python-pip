281cff7702afb809844bcdb749851a7852a228c0
Alberto Duque Villegas <jalbertoduque@yahoo.com>
2023-10-13 22:56:59 -0500

    Actualizo main y store de python con los comentarios hechos

--- web-server/main.py
+++ web-server/main.py
@@ -1,8 +1,11 @@
-import store
+# Este es nuestro ejecutable del proyecto
+
+import store # Importamos el módulo que contiene la función e imprime los resultados de la solicitud
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+# Por ahora comento la siguiente línea:
+#app = FastAPI()
 
 @app.get('/')
 def get_list():
@@ -15,8 +18,10 @@ def get_list():
         <p>soy un parrafo</p>
     """
 
-def run():
-    store.get_categories()
+def run(): #Definimos una función
+    store.get_categories() # Que nos trae las categorías, ejecutando la función que creamos en el módulo
+
+# Lo siguiente se incluye porque queremos que se ejecute como un script
 
 if __name__ == '__main__':
-    run()
+    run() # Al correr como script, ejecuta la función run
--- web-server/store.py
+++ web-server/store.py
@@ -1,9 +1,11 @@
-import requests
+# Este es nuestro módulo para hacer las solicitudes en nuestro proyecto
 
-def get_categories():
-    r = requests.get('https://api.escuelajs.co/api/v1/categories')
-    print(r.status_code)
-    print(r.text)
+import requests # importamos para poder hacer llamados request a http
+
+def get_categories(): #Agregamos esta función
+    r = requests.get('https://api.escuelajs.co/api/v1/categories') # Creamos la variable y nos aseguramos de poner la dirección a la que queremos hacer las solicitudes
+    print(r.status_code) # Al hacer la solicitud recibimos el estado
+    print(r.text) # Recibimos el texto (contenido) que nos retorna con la solicitud
     print(type(r.text))
     categories = r.json()
     for category in categories:
